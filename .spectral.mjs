71875e8571fcc4cfe7c2c57a7d5663f83fd6200a
priyabrata.parida@tredence.com <priyabrata.parida@tredence.com>
2025-02-26 16:46:48 +0530

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3MzY1Mw/spectral.js?branch_slug=main&token=8b8f5815-c16a-4ee3-8063-ae185a6faba9";
+export default { extends: ruleset };
